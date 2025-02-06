62535c6c6c5dd75c82ff69d7f5db1f5cab8a9364
Huzaifa <muhammad.huzaifa.tariq@codingkey.com>
2025-02-06 15:40:52 +0500

    Custom Observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Observable, Subscription} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit ,OnDestroy{
+
+  public firstSubscription : Subscription;
 
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
+
+    // this.firstSubscription = interval(1000).subscribe({
+    //   next:(count)=>{
+    //     console.log(count)
+    //   }
+    // });
+
+    const customObservable = Observable.create((observer)=>{
+      let count = 0;
+      setInterval(()=>{
+        observer.next(count);
+        if(count == 5){
+          observer.complete();
+        }
+        if(count>5){
+          observer.error(new Error('Count value greater than 5'));
+        }
+        count++;
+      },1000);
+    });
+
+    this.firstSubscription = customObservable.pipe
+    (map((data :number)=>{
+      return ('Round' + (data + 1));
+    })).subscribe({
+      next:(data)=>{
+        console.log(data);
+      },
+      error:(err)=>{
+        alert(`Error ${err}`);
+      },
+      complete:()=>{
+        console.log('Observable is now complete');
+      }
+    });
+
+  }
+
+  public ngOnDestroy() {
+
+    this.firstSubscription.unsubscribe();
+
   }
 
 }
